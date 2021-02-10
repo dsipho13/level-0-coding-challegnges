e6283404e8046a59c10215265f85b93ad3d9f00c
unknown <dsipho13@gmail.com>
2021-02-10 08:36:45 +0200

    added Task0.2

---
+++ Task 0.2.py
@@ -0,0 +1,10 @@
+x = 1 + 1 * 2 
+print(x)
+y = (1 + 1) * 2 # this multiplies two to the sum 
+print(y)
+z = 1 + (1 * 2) # adds one to the product
+print(z)
+a = 1 + 1 * 2 / 2  
+print(a)
+b = (1 + 1 * 2) / 2 # divides the expression in the bracket by 2
+print(b)
