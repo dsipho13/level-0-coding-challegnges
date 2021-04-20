7a42895ec29e8b0ba943025f1ff1e77c27150de7
dsipho13 <dsipho13@gmail.com>
2021-04-20 13:03:32 +0200

    made even more changes

--- Task 0.9.py
+++ Task 0.9.py
@@ -1,15 +1,14 @@
-def print_vowels(x):
-    word = str(x)
+def vowels(word):
     vowel_list = [ "A","E","I","O","U","a","e","i","o","u"]
     vowel = ""
-    for i in range(len(word)):
-        if word[i] in vowel_list and word[i] not in vowel:
-            vowel = vowel + word[i]
+    coma =","
+    for i in word:
+        if i in vowel_list and i not in vowel:
+            vowel = vowel + i
 
     if vowel == "":
         print("There are no vowels in your word")
     else:
-        print("The vowels are: " + vowel)
+        print(f"{coma.join(vowel)}")
 
-
-print_vowels("yyhreuieasuouioEAOUIIOYOUg")
+vowels("yyhreuieasuouioEAOUIIOYOUg")
