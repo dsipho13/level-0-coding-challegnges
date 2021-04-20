70812ebe1e496788d417bd090a553263dfee55fe
dsipho13 <dsipho13@gmail.com>
2021-04-20 12:41:37 +0200

    made changes

--- Task 0.8.py
+++ Task 0.8.py
@@ -1,18 +1,12 @@
-def func(i):
-    j = i / 60
-    hour = j // 1
-    hour = int(hour)
-    hour_to_minute = hour * 60
-    minute = i - hour_to_minute
-    minute = int(minute)
+def toHours(time):
+    hour = time // 60
+    minute = time - (hour * 60)
     strhour = " hour"
     strmin = " minute"
     if( hour != 1):
         strhour += "s"
     if( minute != 1):
         strmin += "s"
-    print(str(hour) + strhour +" and " + str(minute) + strmin)
+    print(f"{hour} {strhour} and {minute} {strmin}")
 
-time = int(input("Please enter a number"))
-
-func(time)
+toHours(121)
