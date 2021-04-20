b59eda232b858853cac0a9c269930d72e446f705
dsipho13 <dsipho13@gmail.com>
2021-04-20 12:00:39 +0200

    added changes

--- Task 0.5.py
+++ Task 0.5.py
@@ -1,7 +1,7 @@
-def area_of_triangle(x,y,r):
-    semi_parameter = (x + y + r)/2 
-    area = (semi_parameter * (semi_parameter - x)*(semi_parameter - y)*(semi_parameter -r) )**(0.5) # Heron's formula
+def triangleArea(x,y,r):
+    semiparam = (x + y + r)/2 
+    area = (semiparam * (semiparam - x)*(semiparam - y)*(semiparam -r) )**(0.5) # calculate the aread
     return area 
 
-print(area_of_triangle(3,3,3))
+print(triangleArea(3,3,3))
 
