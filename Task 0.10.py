398ef2b2b8d5fcec4ece05ef48b6725f9c1bff60
dsipho13 <dsipho13@gmail.com>
2021-04-20 13:11:42 +0200

    added more chnages again

--- Task 0.10.py
+++ Task 0.10.py
@@ -1,13 +1,10 @@
 def common_letters(x,y):
-    """ a function that returns common letters
-    """
-    x = str(x)
-    y = str(y)
     common_char = ""
+    coma = ","
     for i in range(len(x)):
         if x[i] in y and x[i] not in common_char:
-            common_char = common_char + x[i] + ", "
+            common_char = common_char + x[i]
     
-    print(common_char[:-2])
+    print(coma.join(common_char))
 
-common_letters("common","column")
+common_letters("conduct","sodom")
