6eacf302436226c3f69296728999e7d1828c5dea
dsipho13 <dsipho13@gmail.com>
2021-04-23 09:30:52 +0200

    more changes

--- Task 0_10.py
+++ Task 0_10.py
@@ -1,10 +1,9 @@
-def common_letters(string1,string2):
+def common_letters(string_1,string_2):
     common_char = ""
     coma = ", "
-    for i in range(len(string1)):
-        if string1[i] in string2 and string1[i] not in common_char:
-            common_char = common_char + string1[i]
-    
+    for i in range(len(string_1)):
+        if string_1[i] in string_2 and string_1[i] not in common_char:
+            common_char = common_char + string_1[i]
     print(coma.join(common_char))
 
 def main():
--- Task 0_5.py
+++ Task 0_5.py
@@ -1,10 +1,10 @@
-def triangleArea(x,y,r):
-    semiparam = (x + y + r)/2 
-    area = (semiparam * (semiparam - x)*(semiparam - y)*(semiparam -r) )**(0.5) #Heron's formula
+def triangle_area(length,height,width):
+    semiparam = (length + height + width)/2 
+    area = (semiparam * (semiparam - length)*(semiparam - height)*(semiparam -width) )**(0.5) #Heron's formula
     return area 
 
 def main():
-    print(triangleArea(3,3,3))
+    print(triangle_area(3,3,3))
 
 if __name__ == "__main__":
     main()
