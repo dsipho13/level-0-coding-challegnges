edf5884661b5c6469856f2dbc094e0c66735256d
dsipho13 <dsipho13@gmail.com>
2021-05-25 09:51:29 +0200

    Added changes

--- task10.py
+++ task10.py
@@ -1,6 +1,8 @@
 def common_letters(string_1, string_2):
     common_char = ""
     coma = ", "
+    string_1 = string_1.lower()
+    string_2 = string_2.lower()
     for i in range(len(string_1)):
         if string_1[i] in string_2 and string_1[i] not in common_char:
             common_char = common_char + string_1[i]
@@ -8,7 +10,7 @@ def common_letters(string_1, string_2):
 
 
 def main():
-    common_letters("conduct", "sodom")
+    common_letters("cOnduct", "sodom")
 
 
 if __name__ == "__main__":
