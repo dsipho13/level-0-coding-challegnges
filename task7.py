2ec49fa11848889aeaef018a14d8c5e49174bcc5
dsipho13 <dsipho13@gmail.com>
2021-05-25 06:20:56 +0200

    changes made

--- task7.py
+++ task7.py
@@ -1,16 +1,16 @@
-def convertToF(temperature):
+def convert_to_farenheit(temperature):
     farenheit = (9 / 5) * temperature + 32
     return farenheit
 
 
-def convertToC(temperature):
+def convert_to_celcius(temperature):
     celcius = (temperature - 32) * (5 / 9)
     return celcius
 
 
 def main():
-    print(convertToF(28))
-    print(convertToC(convertToF(28)))
+    print(convert_to_farenheit(28))
+    print(convert_to_celcius(convert_to_farenheit(28)))
 
 
 if __name__ == "__main__":
